9b85a7e27d1b470bb4af99fd87210802e1fce3b8
vinayakekbote <vinayakekbote12@gmail.com>
2026-07-11 17:11:28 +0530

    Microservice and OpenFeign, Eureka call Enable and Api changes

--- src/main/java/com/digitalwallet/wallet/service/WalletService.java
+++ src/main/java/com/digitalwallet/wallet/service/WalletService.java
@@ -2,6 +2,7 @@ package com.digitalwallet.wallet.service;
 
 import com.digitalwallet.wallet.dto.requestDto.CreditRequestDto;
 import com.digitalwallet.wallet.dto.requestDto.WalletCreateRequestDto;
+import com.digitalwallet.wallet.dto.requestDto.WalletDto;
 import com.digitalwallet.wallet.dto.requestDto.WithdrawRequestDto;
 import com.digitalwallet.wallet.util.CommonResponse;
 
@@ -10,7 +11,7 @@ public interface WalletService {
 
     CommonResponse<String> createWallet(WalletCreateRequestDto requestDto);
 
-    CommonResponse<String> depositMoney(CreditRequestDto requestDto);
+    CommonResponse<WalletDto> depositMoney(CreditRequestDto requestDto);
 
-    CommonResponse<String> withdrawMoney(WithdrawRequestDto requestDto);
+    CommonResponse<WalletDto> withdrawMoney(WithdrawRequestDto requestDto);
 }
